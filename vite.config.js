0ac0c177c3943d8ff9a9f2dffd354a2eb151cb2d
safa EL-ALAMI <safaelalami971@gmail.com>
2025-01-12 00:59:34 +0000

    Add gh-pages configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css : {
-    modules:{
-      localsConvention: "camelCase"
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+  build: {
+    // Increase the chunk size warning limit
+    chunkSizeWarningLimit: 1000, // You can adjust this if needed
 
-    }
-  }
-})
+    // Optional: Add manualChunks configuration for custom chunking
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Move libraries like 'react', 'react-dom', or others into separate chunks
+          vendor: ['react', 'react-dom', 'lodash', 'axios'],
+        },
+      },
+    },
+  },
+});
